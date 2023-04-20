5857a0bb35813980498ddd5a7748a2469fbefb91
Lee E. Hinman <lee.e.hinman@elastic.co>
2023-04-20 15:57:02 -0500

    changing type of Fields and Meta, add other encoders

--- benchmark/proto_test.go
+++ benchmark/proto_test.go
@@ -1,7 +1,12 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
 package benchmark
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -12,102 +17,114 @@ import (
 	"github.com/elastic/elastic-agent-libs/mapstr"
 	"github.com/elastic/elastic-agent-shipper-client/pkg/helpers"
 	"github.com/elastic/elastic-agent-shipper-client/pkg/proto/messages"
+	"github.com/elastic/go-structform/cborl"
+	"github.com/elastic/go-structform/gotype"
+	fxamacker "github.com/fxamacker/cbor/v2"
+	goccy "github.com/goccy/go-json"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func mapStrToEvent(m mapstr.M) (*messages.Event, error) {
-	tsString, err := m.GetValue("@timestamp")
-	if err != nil {
-		return nil, fmt.Errorf("could not get @timestamp: %w", err)
-	}
-	err = m.Delete("@timestamp")
-	if err != nil {
-		return nil, fmt.Errorf("could not remove @timestamp")
-	}
-	ts, err := time.Parse(time.RFC3339, tsString.(string))
-	if err != nil {
-		return nil, fmt.Errorf("could not parse timestamp: %w", err)
-	}
+type ShallowEvent struct {
+	Timestamp  string     `json:"timestamp"`
+	Source     Source     `json:"source"`
+	DataStream DataStream `json:"data_stream"`
+	Metadata   []byte     `json:"metadata"`
+	Fields     []byte     `json:"fields"`
+}
 
-	metaMpstr, err := m.GetValue("@metadata")
-	if err != nil {
-		return nil, fmt.Errorf("could not get @metadata: %w", err)
-	}
-	meta, err := helpers.NewValue(metaMpstr)
-	if err != nil {
-		return nil, fmt.Errorf("failed to conver metadata to protobuf: %w", err)
-	}
-	err = m.Delete("@metadata")
-	if err != nil {
-		return nil, fmt.Errorf("failed to delete @metadata: %w", err)
-	}
-	fields, err := helpers.NewValue(m)
-	if err != nil {
-		return nil, fmt.Errorf("failed to convert fields to protobuf: %w", err)
-	}
+type DeepEvent struct {
+	Timestamp  string     `json:"timestamp"`
+	Source     Source     `json:"source"`
+	DataStream DataStream `json:"data_stream"`
+	Metadata   mapstr.M   `json:"metadata"`
+	Fields     mapstr.M   `json:"fields"`
+}
 
-	return &messages.Event{
-		Timestamp: timestamppb.New(ts),
-		Metadata:  meta.GetStructValue(),
-		Fields:    fields.GetStructValue(),
-		Source: &messages.Source{
+type Source struct {
+	InputId  string `json:"input_id"`
+	StreamId string `json:"stream_id"`
+}
+type DataStream struct {
+	Type      string `json:"type"`
+	Dataset   string `json:"dataset"`
+	Namespace string `json:"namespace"`
+}
+
+func readNdjson(filename string) ([][]byte, error) {
+	out := [][]byte{}
+	fp := filepath.Join("testdata", filename)
+	fileHandle, err := os.Open(fp)
+	if err != nil {
+		return nil, fmt.Errorf("could not open file: %s: %w", fp, err)
+	}
+	defer fileHandle.Close()
+	scanner := bufio.NewScanner(fileHandle)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		tmp := scanner.Bytes()
+		tmp2 := make([]byte, len(tmp))
+		copy(tmp2, tmp)
+		out = append(out, tmp2)
+	}
+	return out, nil
+}
+
+func bytesToMessagesEvents(input [][]byte) ([]*messages.Event, error) {
+	events := []*messages.Event{}
+	for _, raw := range input {
+		m := mapstr.M{}
+		if err := json.Unmarshal(raw, &m); err != nil {
+			return events, fmt.Errorf("could not unmarshal: %w: %v", err, string(raw))
+		}
+		event := messages.Event{}
+		tsString, err := m.GetValue("@timestamp")
+		if err != nil {
+			return events, fmt.Errorf("could not get @timestamp: %w", err)
+		}
+		err = m.Delete("@timestamp")
+		if err != nil {
+			return events, fmt.Errorf("could not remove @timestamp")
+		}
+		ts, err := time.Parse(time.RFC3339, tsString.(string))
+		if err != nil {
+			return events, fmt.Errorf("could not parse timestamp: %w", err)
+		}
+		metaMpstr, err := m.GetValue("@metadata")
+		if err != nil {
+			return events, fmt.Errorf("could not get @metadata: %w", err)
+		}
+		meta, err := helpers.NewValue(metaMpstr)
+		if err != nil {
+			return nil, fmt.Errorf("failed to conver metadata to protobuf: %w", err)
+		}
+		err = m.Delete("@metadata")
+		if err != nil {
+			return nil, fmt.Errorf("failed to delete @metadata: %w", err)
+		}
+		fields, err := helpers.NewValue(m)
+		if err != nil {
+			return nil, fmt.Errorf("failed to convert fields to protobuf: %w", err)
+		}
+
+		event.Timestamp = timestamppb.New(ts)
+		event.Metadata = meta.GetStructValue()
+		event.Fields = fields.GetStructValue()
+		event.Source = &messages.Source{
 			InputId:  "inputID",
 			StreamId: "streamID",
-		},
-		DataStream: &messages.DataStream{
+		}
+		event.DataStream = &messages.DataStream{
 			Type:      "log",
 			Namespace: "default",
 			Dataset:   "generic",
-		},
-	}, nil
-}
-
-func ndjsonToEvents(filename string) ([]*messages.Event, error) {
-	events := []*messages.Event{}
-	fp := filepath.Join("testdata", filename)
-	fileHandle, err := os.Open(fp)
-	if err != nil {
-		return events, fmt.Errorf("could not open file: %s: %w", fp, err)
-	}
-	defer fileHandle.Close()
-	scanner := bufio.NewScanner(fileHandle)
-	scanner.Split(bufio.ScanLines)
-	for scanner.Scan() {
-		m := mapstr.M{}
-		if err := json.Unmarshal(scanner.Bytes(), &m); err != nil {
-			return events, fmt.Errorf("could not unmarshal: %w", err)
 		}
-		e, err := mapStrToEvent(m)
-		if err != nil {
-			return events, fmt.Errorf("could not convert mapstr to event: %w", err)
-		}
-		events = append(events, e)
+		events = append(events, &event)
 	}
 	return events, nil
 }
 
-func ndjsonToMapStr(filename string) ([]*mapstr.M, error) {
-	events := []*mapstr.M{}
-	fp := filepath.Join("testdata", filename)
-	fileHandle, err := os.Open(fp)
-	if err != nil {
-		return events, fmt.Errorf("could not open file: %s: %w", fp, err)
-	}
-	defer fileHandle.Close()
-	scanner := bufio.NewScanner(fileHandle)
-	scanner.Split(bufio.ScanLines)
-	for scanner.Scan() {
-		m := mapstr.M{}
-		if err := json.Unmarshal(scanner.Bytes(), &m); err != nil {
-			return events, fmt.Errorf("could not unmarshal: %w", err)
-		}
-		events = append(events, &m)
-	}
-	return events, nil
-}
-
-func roundtripEvent(m *messages.Event) {
+func rtMessagesEvent(m *messages.Event) {
 	b, err := proto.Marshal(m)
 	if err != nil {
 		panic(err)
@@ -119,7 +136,19 @@ func roundtripEvent(m *messages.Event) {
 	}
 }
 
-func roundtripMapStr(m *mapstr.M) {
+func bytesToMapStr(input [][]byte) ([]*mapstr.M, error) {
+	events := []*mapstr.M{}
+	for _, raw := range input {
+		m := mapstr.M{}
+		if err := json.Unmarshal(raw, &m); err != nil {
+			return events, fmt.Errorf("could not unmarshal: %w: %v", err, string(raw))
+		}
+		events = append(events, &m)
+	}
+	return events, nil
+}
+
+func rtMapStrStdJSON(m *mapstr.M) {
 	b, err := json.Marshal(m)
 	if err != nil {
 		panic(err)
@@ -131,54 +160,457 @@ func roundtripMapStr(m *mapstr.M) {
 	}
 }
 
-func BenchmarkEventSingleField(b *testing.B) {
-	events, err := ndjsonToEvents("message.ndjson")
+func bytesToMessagesShallowEvents(input [][]byte) ([]*messages.ShallowEvent, error) {
+	events := []*messages.ShallowEvent{}
+	for _, raw := range input {
+		m := mapstr.M{}
+		if err := json.Unmarshal(raw, &m); err != nil {
+			return events, fmt.Errorf("could not unmarshal: %w: %v", err, string(raw))
+		}
+		event := messages.ShallowEvent{}
+		tsString, err := m.GetValue("@timestamp")
+		if err != nil {
+			return events, fmt.Errorf("could not get @timestamp: %w", err)
+		}
+		err = m.Delete("@timestamp")
+		if err != nil {
+			return events, fmt.Errorf("could not remove @timestamp")
+		}
+		ts, err := time.Parse(time.RFC3339, tsString.(string))
+		if err != nil {
+			return events, fmt.Errorf("could not parse timestamp: %w", err)
+		}
+		metaMpstr, err := m.GetValue("@metadata")
+		if err != nil {
+			return events, fmt.Errorf("could not get @metadata: %w", err)
+		}
+		err = m.Delete("@metadata")
+		metaBytes, err := json.Marshal(metaMpstr)
+		if err != nil {
+			return events, fmt.Errorf("could not marshal meta map string: %w", err)
+		}
+		fieldsBytes, err := json.Marshal(m)
+		if err != nil {
+			return nil, fmt.Errorf("could not marshal fields map string: %w", err)
+		}
+		event.Timestamp = timestamppb.New(ts)
+		event.Metadata = metaBytes
+		event.Fields = fieldsBytes
+		event.Source = &messages.Source{
+			InputId:  "inputID",
+			StreamId: "streamID",
+		}
+		event.DataStream = &messages.DataStream{
+			Type:      "log",
+			Namespace: "default",
+			Dataset:   "generic",
+		}
+		events = append(events, &event)
+	}
+	return events, nil
+}
+
+func rtMessagesShallowEvent(m *messages.ShallowEvent) {
+	b, err := proto.Marshal(m)
 	if err != nil {
 		panic(err)
 	}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		for _, e := range events {
-			roundtripEvent(e)
-		}
+	new := messages.ShallowEvent{}
+	err = proto.Unmarshal(b, &new)
+	if err != nil {
+		panic(err)
 	}
 }
 
-func BenchmarkEventMultiField(b *testing.B) {
-	events, err := ndjsonToEvents("small_json.ndjson")
+func rtMessagesShallowEventFull(m *messages.ShallowEvent) {
+	b, err := proto.Marshal(m)
 	if err != nil {
 		panic(err)
 	}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		for _, e := range events {
-			roundtripEvent(e)
-		}
+	new := messages.ShallowEvent{}
+	err = proto.Unmarshal(b, &new)
+	if err != nil {
+		panic(err)
+	}
+	fields := mapstr.M{}
+	err = json.Unmarshal([]byte(new.Fields), &fields)
+	if err != nil {
+		panic(err)
+	}
+	b, err = json.Marshal(fields)
+	if err != nil {
+		panic(err)
+	}
+	meta := mapstr.M{}
+	err = json.Unmarshal([]byte(new.Metadata), &meta)
+	if err != nil {
+		panic(err)
+	}
+	b, err = json.Marshal(meta)
+	if err != nil {
+		panic(err)
 	}
 }
 
-func BenchmarkMapStrSingleField(b *testing.B) {
-	events, err := ndjsonToMapStr("message.ndjson")
+func rtMessagesShallowEventFullGoJSON(m *messages.ShallowEvent) {
+	b, err := proto.Marshal(m)
 	if err != nil {
 		panic(err)
 	}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		for _, e := range events {
-			roundtripMapStr(e)
-		}
+	new := messages.ShallowEvent{}
+	err = proto.Unmarshal(b, &new)
+	if err != nil {
+		panic(err)
+	}
+	fields := mapstr.M{}
+	err = goccy.Unmarshal([]byte(new.Fields), &fields)
+	if err != nil {
+		panic(err)
+	}
+	b, err = goccy.Marshal(fields)
+	if err != nil {
+		panic(err)
+	}
+	meta := mapstr.M{}
+	err = goccy.Unmarshal([]byte(new.Metadata), &meta)
+	if err != nil {
+		panic(err)
+	}
+	b, err = goccy.Marshal(meta)
+	if err != nil {
+		panic(err)
 	}
 }
 
-func BenchmarkMapStrMultiField(b *testing.B) {
-	events, err := ndjsonToMapStr("small_json.ndjson")
+func rtMapStrGoJSON(m *mapstr.M) {
+	b, err := goccy.Marshal(m)
 	if err != nil {
 		panic(err)
 	}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		for _, e := range events {
-			roundtripMapStr(e)
+	new := mapstr.M{}
+	err = goccy.Unmarshal(b, &new)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func bytesToShallowEvents(input [][]byte) ([]*ShallowEvent, error) {
+	events := []*ShallowEvent{}
+	for _, raw := range input {
+		m := mapstr.M{}
+		if err := json.Unmarshal(raw, &m); err != nil {
+			return events, fmt.Errorf("could not unmarshal: %w: %v", err, string(raw))
 		}
+		event := ShallowEvent{}
+		tsString, err := m.GetValue("@timestamp")
+		if err != nil {
+			return events, fmt.Errorf("could not get @timestamp: %w", err)
+		}
+		err = m.Delete("@timestamp")
+		if err != nil {
+			return events, fmt.Errorf("could not remove @timestamp")
+		}
+		metaMpstr, err := m.GetValue("@metadata")
+		if err != nil {
+			return events, fmt.Errorf("could not get @metadata: %w", err)
+		}
+		err = m.Delete("@metadata")
+		metaBytes, err := json.Marshal(metaMpstr)
+		if err != nil {
+			return events, fmt.Errorf("could not marshal meta map string: %w", err)
+		}
+		fieldsBytes, err := json.Marshal(m)
+		if err != nil {
+			return nil, fmt.Errorf("could not marshal fields map string: %w", err)
+		}
+		event.Timestamp = tsString.(string)
+		event.Metadata = metaBytes
+		event.Fields = fieldsBytes
+		event.Source = Source{
+			InputId:  "inputID",
+			StreamId: "streamID",
+		}
+		event.DataStream = DataStream{
+			Type:      "log",
+			Namespace: "default",
+			Dataset:   "generic",
+		}
+		events = append(events, &event)
+	}
+	return events, nil
+}
+
+func rtShallowEventStdJSON(e *ShallowEvent) {
+	b, err := json.Marshal(e)
+	if err != nil {
+		panic(err)
+	}
+	new := ShallowEvent{}
+	err = json.Unmarshal(b, &new)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func rtShallowEventGoJSON(e *ShallowEvent) {
+	b, err := goccy.Marshal(e)
+	if err != nil {
+		panic(err)
+	}
+	new := ShallowEvent{}
+	err = goccy.Unmarshal(b, &new)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func bytesToDeepEvents(input [][]byte) ([]*DeepEvent, error) {
+	events := []*DeepEvent{}
+	for _, raw := range input {
+		m := mapstr.M{}
+		if err := json.Unmarshal(raw, &m); err != nil {
+			return events, fmt.Errorf("could not unmarshal: %w: %v", err, string(raw))
+		}
+		event := DeepEvent{}
+		tsString, err := m.GetValue("@timestamp")
+		if err != nil {
+			return events, fmt.Errorf("could not get @timestamp: %w", err)
+		}
+		err = m.Delete("@timestamp")
+		if err != nil {
+			return events, fmt.Errorf("could not remove @timestamp")
+		}
+		metaMpstr, err := m.GetValue("@metadata")
+		if err != nil {
+			return events, fmt.Errorf("could not get @metadata: %w", err)
+		}
+		err = m.Delete("@metadata")
+
+		event.Timestamp = tsString.(string)
+		event.Metadata = metaMpstr.(map[string]interface{})
+		event.Fields = m
+		event.Source = Source{
+			InputId:  "inputID",
+			StreamId: "streamID",
+		}
+		event.DataStream = DataStream{
+			Type:      "log",
+			Namespace: "default",
+			Dataset:   "generic",
+		}
+		events = append(events, &event)
+	}
+	return events, nil
+}
+
+func rtDeepEventStdJSON(e *DeepEvent) {
+	b, err := json.Marshal(e)
+	if err != nil {
+		panic(err)
+	}
+	new := DeepEvent{}
+	err = json.Unmarshal(b, &new)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func rtDeepEventStructFormCBORL(e *DeepEvent) {
+	var buf bytes.Buffer
+	visitor := cborl.NewVisitor(&buf)
+	folder, _ := gotype.NewIterator(visitor)
+	folder.Fold(*e)
+
+	unfolder, _ := gotype.NewUnfolder(nil)
+	parser := cborl.NewParser(unfolder)
+	new := DeepEvent{}
+	err := unfolder.SetTarget(&new)
+	if err != nil {
+		panic(err)
+	}
+	parser.Parse(buf.Bytes())
+}
+
+func rtDeepEventGoJSON(e *DeepEvent) {
+	b, err := goccy.Marshal(e)
+	if err != nil {
+		panic(err)
+	}
+	new := DeepEvent{}
+	err = goccy.Unmarshal(b, &new)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func rtDeepEventCBORL(e *DeepEvent) {
+	b, err := fxamacker.Marshal(e)
+	if err != nil {
+		panic(err)
+	}
+	new := DeepEvent{}
+	err = fxamacker.Unmarshal(b, &new)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func BenchmarkMarshalUnmarshal(b *testing.B) {
+	benchmarks := []struct {
+		name     string
+		filename string
+	}{
+		{"Single", "message.ndjson"},
+		{"Multi", "small_json.ndjson"},
+	}
+	for _, bm := range benchmarks {
+		rawBytes, err := readNdjson(bm.filename)
+		if err != nil {
+			panic(err)
+		}
+		b.Run(bm.name+"OriginalProtobuf", func(b *testing.B) {
+			events, err := bytesToMessagesEvents(rawBytes)
+			if err != nil {
+				panic(err)
+			}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				for _, e := range events {
+					rtMessagesEvent(e)
+				}
+			}
+		})
+		b.Run(bm.name+"MapStrStdJSON", func(b *testing.B) {
+			events, err := bytesToMapStr(rawBytes)
+			if err != nil {
+				panic(err)
+			}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				for _, e := range events {
+					rtMapStrStdJSON(e)
+				}
+			}
+		})
+		b.Run(bm.name+"ShallowProtobuf", func(b *testing.B) {
+			events, err := bytesToMessagesShallowEvents(rawBytes)
+			if err != nil {
+				panic(err)
+			}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				for _, e := range events {
+					rtMessagesShallowEvent(e)
+				}
+			}
+		})
+		b.Run(bm.name+"ShallowProtobufFull", func(b *testing.B) {
+			events, err := bytesToMessagesShallowEvents(rawBytes)
+			if err != nil {
+				panic(err)
+			}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				for _, e := range events {
+					rtMessagesShallowEventFull(e)
+				}
+			}
+		})
+		b.Run(bm.name+"ShallowProtobufFullGoJSON", func(b *testing.B) {
+			events, err := bytesToMessagesShallowEvents(rawBytes)
+			if err != nil {
+				panic(err)
+			}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				for _, e := range events {
+					rtMessagesShallowEventFullGoJSON(e)
+				}
+			}
+		})
+		b.Run(bm.name+"MapStrGoJSON", func(b *testing.B) {
+			events, err := bytesToMapStr(rawBytes)
+			if err != nil {
+				panic(err)
+			}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				for _, e := range events {
+					rtMapStrGoJSON(e)
+				}
+			}
+		})
+		b.Run(bm.name+"ShallowEventStdJSON", func(b *testing.B) {
+			events, err := bytesToShallowEvents(rawBytes)
+			if err != nil {
+				panic(err)
+			}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				for _, e := range events {
+					rtShallowEventStdJSON(e)
+				}
+			}
+		})
+		b.Run(bm.name+"ShallowEventGoJSON", func(b *testing.B) {
+			events, err := bytesToShallowEvents(rawBytes)
+			if err != nil {
+				panic(err)
+			}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				for _, e := range events {
+					rtShallowEventGoJSON(e)
+				}
+			}
+		})
+		b.Run(bm.name+"DeepEventStdJSON", func(b *testing.B) {
+			events, err := bytesToDeepEvents(rawBytes)
+			if err != nil {
+				panic(err)
+			}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				for _, e := range events {
+					rtDeepEventStdJSON(e)
+				}
+			}
+		})
+		b.Run(bm.name+"DeepEventSructFormCBORL", func(b *testing.B) {
+			events, err := bytesToDeepEvents(rawBytes)
+			if err != nil {
+				panic(err)
+			}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				for _, e := range events {
+					rtDeepEventStructFormCBORL(e)
+				}
+			}
+		})
+		b.Run(bm.name+"DeepEventGoJSON", func(b *testing.B) {
+			events, err := bytesToDeepEvents(rawBytes)
+			if err != nil {
+				panic(err)
+			}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				for _, e := range events {
+					rtDeepEventGoJSON(e)
+				}
+			}
+		})
+		b.Run(bm.name+"DeepEventCBORL", func(b *testing.B) {
+			events, err := bytesToDeepEvents(rawBytes)
+			if err != nil {
+				panic(err)
+			}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				for _, e := range events {
+					rtDeepEventCBORL(e)
+				}
+			}
+		})
 	}
 }
--- go.mod
+++ go.mod
@@ -13,19 +13,28 @@ require (
 )
 
 require (
+	github.com/aclements/go-moremath v0.0.0-20210112150236-f10218a38794 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/elastic/go-structform v0.0.10 // indirect
 	github.com/elastic/go-ucfg v0.8.5 // indirect
+	github.com/fxamacker/cbor/v2 v2.4.0 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
 	go.elastic.co/ecszap v1.0.1 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/goleak v1.1.12 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/perf v0.0.0-20230227161431-f7320a6d63e8 // indirect
 	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
